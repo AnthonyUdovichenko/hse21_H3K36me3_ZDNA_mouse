0bfe7ee7e78ed72cdcdd3e8352737d09f09dadb6
Anthony Udovichenko <antonyud2000@yandex.ru>
2021-05-20 14:58:00 +0300

    Chip_seeker plots

---
+++ src/chip_seeker.R
@@ -0,0 +1,28 @@
+setwd("C:/Bioinformatics/project/github/hse21_H3K36me3_ZDNA_mouse/src")
+
+source('lib.R')
+
+library(ChIPseeker)
+library(TxDb.Mmusculus.UCSC.mm9.knownGene)
+library(clusterProfiler)
+
+###
+
+# NAME <- 'H3K36me3_CH12.LX.ENCFF493MPV.mm9.filtered'
+# NAME <- 'H3K36me3_CH12.LX.ENCFF827GCP.mm9.filtered'
+BED_FN <- paste0(DATA_DIR, NAME, '.bed')
+
+###
+
+txdb <- TxDb.Mmusculus.UCSC.mm9.knownGene
+
+peakAnno <- annotatePeak(BED_FN, tssRegion=c(-3000, 3000), TxDb=txdb)
+
+pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.pdf'))
+plotAnnoPie(peakAnno)
+dev.off()
+
+peak <- readPeakFile(BED_FN)
+pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.covplot.pdf'))
+covplot(peak, weightCol="V5")
+dev.off()
