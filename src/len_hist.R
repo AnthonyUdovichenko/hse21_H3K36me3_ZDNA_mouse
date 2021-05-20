2ccf2b7a2ccdb55db0c4716deb7ec86bab77c43b
Anthony Udovichenko <antonyud2000@yandex.ru>
2021-05-20 12:09:34 +0300

    Histogram of lengths

--- src/len_hist.R
+++ src/len_hist.R
@@ -11,23 +11,13 @@ source('lib.R')
 
 ###
 
-bed_df <- read.delim(paste0('data/', NAME, '.bed'), as.is = TRUE, header = FALSE)
+bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
 colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
 bed_df$len <- bed_df$end - bed_df$start
-head(bed_df)
-
-bed_df <- bed_df %>%
-  arrange(-len) %>%
-  filter(len < 50000)
 
 ggplot(bed_df) +
   aes(x = len) +
   geom_histogram() +
   ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
   theme_bw()
-ggsave(paste0('len_hist.', NAME, '.filtered.pdf'), path = OUT_DIR)
-
-bed_df %>%
-  select(-len) %>%
-  write.table(file='data/H3K36me3_CH12.LX.ENCFF827GCP.mm10.filtered.bed',
-              col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
+ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
