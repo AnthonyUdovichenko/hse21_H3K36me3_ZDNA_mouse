04637ceeb597cb5a771b8e8673770eb5ebe34bf8
Anthony Udovichenko <antonyud2000@yandex.ru>
2021-05-27 12:03:57 +0300

    deepZ histogram

--- src/len_hist.R
+++ src/len_hist.R
@@ -4,6 +4,7 @@ source('lib.R')
 
 ###
 
+NAME <- 'mouseZ-DNA1'
 # NAME <- 'H3K36me3_CH12.LX.ENCFF493MPV.mm9'
 # NAME <- 'H3K36me3_CH12.LX.ENCFF493MPV.mm10'
 # NAME <- 'H3K36me3_CH12.LX.ENCFF827GCP.mm9'
@@ -12,7 +13,8 @@ source('lib.R')
 ###
 
 bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
-colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+# colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score', 'other1', 'other2', 'other3', 'other4', 'other5')
 bed_df$len <- bed_df$end - bed_df$start
 
 ggplot(bed_df) +
